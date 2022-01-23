debe0cf7dc864c176a06f00e4e673d97a78418b1
Nick Glas <nickglas@hotmail.nl>
2022-01-23 14:12:16 +0100

    added news filtering

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Typography, Row, Col, Avatar, Card} from 'antd'
+import {React, useEffect, useState} from 'react';
+import {Input, Typography, Row, Col, Avatar, Card} from 'antd'
 import moment from 'moment';
 
 import {useGetCryptoNewsQuery} from '../services/cryptoNewsApi'
@@ -9,39 +9,56 @@ const {Text, Title} = Typography
 const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg'
 
 const News = ({simplified}) => {
+
   const {data: cryptoNews, isFetching } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [news, setNews] = useState([]);
+
+  useEffect(()=>{
+    const filteredData = cryptoNews?.value?.filter((article) => article.name.toLowerCase().includes(searchTerm));
+    setNews(filteredData)
+  },[searchTerm,cryptoNews])
 
   if(isFetching || !cryptoNews?.value) return "Loading..."
 
 
-
   return(
-    <Row gutter={[24,24]}>
-      {cryptoNews.value.map((article, i) => (
-        <Col xs={24} sm={12} lg={8} key={i}>
-          <Card hoverable className='news-card'>
-            <a href={article.url} target='_blank' rel='noreferrer'>
-              <div className="news-image-container">
-                <Title className='news-title' level={4}>
-                  {article.name}
-                </Title>
-                <img src={article?.image?.thumbnail?.contentUrl || demoImage} alt='article images' style={{maxWidth:'200px', maxHeight:'200px'}}/>
-              </div>
-              <p>
-                {article.description > 100 ? `${article.description.substring(0,100)}...` : article.description}
-              </p>
-              <div className="provider-container">
-                <div>
-                  <Avatar src={article.provider[0]?.image?.thumbnail?.contentUrl || demoImage}/>
-                  <Text className='provider-name'>{article.provider[0]?.name}</Text>
+    <>
+      {!simplified && 
+        <div className='search-crypto'>
+          <Input placeholder='search the news' onChange={(e) => setSearchTerm(e.target.value)}/>
+        </div>
+      }
+      
+      <Row gutter={[24,24]}>
+        {news?.map((article, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className='news-card'>
+              <a href={article.url} target='_blank' rel='noreferrer'>
+                <div className="news-image-container">
+                  <Title className='news-title' level={4}>
+                    {article.name}
+                  </Title>
+                  <img src={article?.image?.thumbnail?.contentUrl || demoImage} alt='article images' style={{maxWidth:'200px', maxHeight:'200px'}}/>
                 </div>
-                <Text>{moment(article.datePublished).startOf('ss').fromNow()}</Text>
-              </div>
-            </a>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+                <p>
+                  {article.description > 100 ? `${article.description.substring(0,100)}...` : article.description}
+                </p>
+                <div className="provider-container">
+                  <div>
+                    <Avatar src={article.provider[0]?.image?.thumbnail?.contentUrl || demoImage}/>
+                    <Text className='provider-name'>{article.provider[0]?.name}</Text>
+                  </div>
+                  <Text>{moment(article.datePublished).startOf('ss').fromNow()}</Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+    
   );
 };
 
